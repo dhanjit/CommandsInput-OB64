b80b3c4eb05ecc84bf2fbb2ab5de03f3a707fc90
dhanjit <dhanjitdas1@gmail.com>
2025-05-11 10:53:21 +0530

    Initial commit

---
+++ lib/commonlibob64/include/OBSE/OBSE.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "OBSE/Impl/PCH.h"
+
+#include "OBSE/API.h"
+#include "OBSE/Interfaces.h"
+#include "OBSE/Trampoline.h"
+#include "OBSE/Version.h"
---
+++ lib/commonlibob64/src/OBSE/Interfaces.cpp
@@ -0,0 +1,65 @@
+#include "OBSE/Interfaces.h"
+
+#include "OBSE/API.h"
+
+#include "REX/W32/KERNEL32.h"
+
+namespace OBSE
+{
+	bool MessagingInterface::RegisterListener(EventCallback* a_handler, stl::zstring a_sender) const
+	{
+		const auto success =
+			GetProxy().RegisterListener(
+				GetPluginHandle(),
+				a_sender.data(),
+				a_handler);
+		if (!success) {
+			REX::ERROR("failed to register listener for {}"sv, a_sender);
+		}
+		return success;
+	}
+
+	bool MessagingInterface::Dispatch(std::uint32_t a_messageType, void* a_data, std::uint32_t a_dataLen, const char* a_receiver) const
+	{
+		const auto success =
+			GetProxy().Dispatch(
+				GetPluginHandle(),
+				a_messageType,
+				a_data,
+				a_dataLen,
+				a_receiver);
+		if (!success) {
+			REX::ERROR("failed to dispatch to {}"sv, (a_receiver ? a_receiver : "all listeners"));
+		}
+		return success;
+	}
+
+	void* TrampolineInterface::AllocateFromBranchPool(std::size_t a_size) const
+	{
+		const auto mem =
+			GetProxy().AllocateFromBranchPool(
+				GetPluginHandle(),
+				a_size);
+		if (!mem) {
+			REX::ERROR("failed to allocate from branch pool"sv);
+		}
+		return mem;
+	}
+
+	void* TrampolineInterface::AllocateFromLocalPool(std::size_t a_size) const
+	{
+		const auto mem =
+			GetProxy().AllocateFromLocalPool(
+				GetPluginHandle(),
+				a_size);
+		if (!mem) {
+			REX::ERROR("failed to allocate from local pool"sv);
+		}
+		return mem;
+	}
+
+	const PluginVersionData* PluginVersionData::GetSingleton() noexcept
+	{
+		return reinterpret_cast<const PluginVersionData*>(REX::W32::GetProcAddress(REX::W32::GetCurrentModule(), "OBSEPlugin_Version"));
+	}
+}
